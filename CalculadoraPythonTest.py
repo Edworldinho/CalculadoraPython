e06fab517b49024233089d150b2fcb90d5a670fa
Edworldinho <64818232+Edworldinho@users.noreply.github.com>
2023-11-24 16:08:42 -0300

    Update CalculadoraPythonTest.py

--- CalculadoraPythonTest.py
+++ CalculadoraPythonTest.py
@@ -3,16 +3,16 @@ from CalculadoraPython import CalculadoraPython
 
 def test_somar():
     calculadorapython = CalculadoraPython(3,3)
-    assert CalculadoraPython.somar(self) == 6
+    assert calculadorapython.somar() == 6
 
 def test_diminuir():
     calculadorapython = CalculadoraPython(2,1)
-    assert CalculadoraPython.diminuir(self) == 1
+    assert calculadorapython.diminuir() == 1
 
 def test_multiplicar():
     calculadorapython = CalculadoraPython(2,4)
-    assert CalculadoraPython.multiplicar(self) == 8
+    assert calculadorapython.multiplicar() == 8
 
 def test_dividir():
     calculadorapython = CalculadoraPython(15,5)
-    assert CalculadoraPython.dividir(self) == 3
+    assert calculadorapython.dividir() == 3
